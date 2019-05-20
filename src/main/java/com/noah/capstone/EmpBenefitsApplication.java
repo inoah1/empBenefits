001c1087bf9151be3b312ce897d808b82b70448a
inoah <inoah2010@gmail.com>
2019-05-19 23:35:15 -0400

    initial commit

---
+++ src/main/java/com/noah/capstone/EmpBenefitsApplication.java
@@ -0,0 +1,13 @@
+package com.noah.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpBenefitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpBenefitsApplication.class, args);
+	}
+
+}
